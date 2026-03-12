fabb6e30f99bf8477b63cdb05d0cac769f78a055
tabswayrecipe <tabswayrecipe@yahoo.com>
2026-03-12 12:36:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let progressBar = document.getElementById("progressBar");
-let connectBtn = document.getElementById("connectBtn");
-
-let width = 0;
-
-// Progress bar animation
-let interval = setInterval(function(){
-  width++;
-  progressBar.style.width = width + "%";
-  if(width >= 100){
-    clearInterval(interval);
-    connectBtn.disabled = false;
-    connectBtn.classList.remove("pulse");
-  }
-}, 100);
-
-// Connect button function
 function connectWifi() {
-  window.location.href = "$authUrl"; // replace with your actual $authUrl
+
+window.location.href="$authUrl";
+
 }
+
+let progressBar=document.getElementById("progressBar");
+let connectBtn=document.getElementById("connectBtn");
+
+let width=0;
+
+let interval=setInterval(function(){
+
+width++;
+
+progressBar.style.width=width+"%";
+
+if(width>=100){
+
+clearInterval(interval);
+connectBtn.disabled=false;
+
+}
+
+},100);
